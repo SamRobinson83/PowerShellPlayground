72ce08e990b7a91c66367587287ca3dba261decf
Sam Robinson <samuelrobinson@gmail.com>
2018-05-09 16:29:21 -0700

    Initial Commit

---
+++ CreateFile.ps1
@@ -0,0 +1,15 @@
+﻿#################################################################
+#################################################################
+#############  File Creation ####################################
+######## By Sam Robinson     ####################################
+#################################################################
+
+
+#Create a file in given directory that is a junk test file
+
+$Directory = Read-Host "Enter directory where you want to create the file:"
+$Filename = Read-Host "Enter name of the file you want to create:"
+$SizeMB = Read-Host "Enter the size of the file you want to create in MB:"
+$SizeBytes = $SizeMB * 1048576
+$FullPath = "$Directory$Filename"
+fsutil file createnew $FullPath $SizeBytes
